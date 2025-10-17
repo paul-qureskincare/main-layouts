2e0abb7ba9650a4de85bf8ac8a9d63d25f03bf29
Alexander Koretsky <okoretsky@gmail.com>
2025-10-17 16:07:59 +0300

    Created new page early access black friday

---
+++ alex/sections/isolated/early-access-black-friday/early-access-black-friday.js
@@ -0,0 +1,24 @@
+const $ = id => document.getElementById(id);
+const [$hours, $minutes, $seconds] = ['ea-hours', 'ea-minutes', 'ea-seconds'].map($);
+
+const pad = n => n.toString().padStart(2, '0');
+
+function update() {
+    const diff = Math.max(0, targetBF - Date.now());
+
+    const hours = Math.floor(diff / 3600000);
+    const minutes = Math.floor((diff % 3600000) / 60000);
+    const seconds = Math.floor((diff % 60000) / 1000);
+
+    $hours.textContent = pad(hours);
+    $minutes.textContent = pad(minutes);
+    $seconds.textContent = pad(seconds);
+
+    if (!diff) {
+        clearInterval(interval);
+        // document.getElementById('early-access-countdown').textContent = 'Event started!';
+    }
+}
+
+update();
+const interval = setInterval(update, 1000);
